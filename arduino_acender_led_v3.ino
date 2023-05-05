8c89c6f5aee65a28c18df2952a355dde7cbc2bbc
André Maliceski <alice.maliceski@gmail.com>
2023-05-05 18:23:52 -0300

    Add files via upload

---
+++ arduino_acender_led_v3.ino
@@ -0,0 +1,21 @@
+int tempo = 0;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  tempo = 1000;
+}
+
+void loop()
+{
+  delay(tempo); // Wait for tempo millisecond(s)
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(tempo); // Wait for tempo millisecond(s)
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(tempo); // Wait for tempo millisecond(s)
+  digitalWrite(12, LOW);
+  delay(tempo); // Wait for tempo millisecond(s)
+  digitalWrite(12, HIGH);
+}
